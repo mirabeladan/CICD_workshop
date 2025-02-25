3e9673c7d5249c28e92c74915c616e8caba7354e
miradd <miradd@amazon.co.uk>
2025-02-25 04:56:44 +0000

    Added Signer Notation & Signer CLI and signed the image pushed to ECR

--- app-cdk/app_cdk/pipeline_cdk_stack.py
+++ app-cdk/app_cdk/pipeline_cdk_stack.py
@@ -8,6 +8,7 @@ from aws_cdk import (
     aws_codebuild as codebuild,
     aws_codepipeline_actions as codepipeline_actions,
     aws_iam as iam,
+    aws_ssm as ssm,
 )
 
 class PipelineCdkStack(Stack):
@@ -48,20 +49,20 @@ class PipelineCdkStack(Stack):
                 environment_variables = {
                     'IMAGE_TAG': codebuild.BuildEnvironmentVariable(
                         type = codebuild.BuildEnvironmentVariableType.PLAINTEXT,
-                        value = 'latest'
+                        value = 'latest',
                     ),
                     'IMAGE_REPO_URI': codebuild.BuildEnvironmentVariable(
                         type = codebuild.BuildEnvironmentVariableType.PLAINTEXT,
-                        value = ecr_repository.repository_uri
+                        value = ecr_repository.repository_uri,
                     ),
                     'AWS_DEFAULT_REGION': codebuild.BuildEnvironmentVariable(
                         type = codebuild.BuildEnvironmentVariableType.PLAINTEXT,
-                        value = os.environ['CDK_DEFAULT_REGION']
+                        value = os.environ['CDK_DEFAULT_REGION'],
                     )
                 }
             ),
         )
-
+   
         docker_build_project.add_to_role_policy(iam.PolicyStatement(
             effect = iam.Effect.ALLOW,
             actions = [
@@ -76,11 +77,37 @@ class PipelineCdkStack(Stack):
                 'ecr:InitiateLayerUpload',
                 'ecr:UploadLayerPart',
                 'ecr:CompleteLayerUpload',
-                'ecr:PutImage'
+                'ecr:PutImage',
             ],
             resources = ['*'],
         ))
 
+        ssmParameter = ssm.StringParameter(
+            self, 'SignerProfileARN',
+            parameter_name='signer-profile-arn',
+            string_value='arn:aws:signer:us-east-2:937871024262:/signing-profiles/ecr_signing_profile'
+        )
+   
+        docker_build_project.add_to_role_policy(iam.PolicyStatement(
+            effect = iam.Effect.ALLOW,
+            actions = [
+                'ssm:GetParametersByPath',
+                'ssm:GetParameters',
+            ],
+            resources = ['*'],
+        ))
+
+
+        docker_build_project.add_to_role_policy(iam.PolicyStatement(
+            effect = iam.Effect.ALLOW,
+            actions = [
+                'signer:PutSigningProfile',
+                'signer:SignPayload',
+                'signer:GetRevocationStatus'
+            ],
+            resources = ['*'],
+        ))
+   
         source_output = codepipeline.Artifact()
         unit_test_output = codepipeline.Artifact()
         docker_build_output = codepipeline.Artifact()
@@ -104,24 +131,24 @@ class PipelineCdkStack(Stack):
             action_name = 'Unit-Test',
             project = code_quality_build,
             input = source_output,  # The build action must use the CodeStarConnectionsSourceAction output as input.
-            outputs = [unit_test_output]
+            outputs = [unit_test_output],
         )
 
         pipeline.add_stage(
             stage_name = 'Code-Unit-Tests',
-            actions = [build_action]
+            actions = [build_action],
         )
 
         docker_build_action = codepipeline_actions.CodeBuildAction(
             action_name = 'Docker-Build',
             project = docker_build_project,
             input = source_output,
-            outputs = [docker_build_output]
+            outputs = [docker_build_output],
         )
 
         pipeline.add_stage(
             stage_name = 'Docker-Push-ECR',
-            actions = [docker_build_action]
+            actions = [docker_build_action],
         )
 
         CfnOutput(
